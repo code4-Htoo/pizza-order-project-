b927e27981bd9ac4662955dbad93f76a21809b3e
Htoo Aung Wai <htooaungwai.prof@gmail.com>
2022-12-05 20:19:56 +0630

    final

--- app/Http/Controllers/API/RouteController.php
+++ app/Http/Controllers/API/RouteController.php
@@ -2,69 +2,73 @@
 
 namespace App\Http\Controllers\API;
 
-use Carbon\Carbon;
-use App\Models\User;
-use App\Models\Product;
-use App\Models\Category;
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\Product;
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
 
 class RouteController extends Controller
 {
     // get all product list
-    public function productList(){
+    public function productList()
+    {
         $products = Product::get();
         $users = User::get();
 
         $data = [
-            'product' => $products ,
-            'user' => $users
+            'product' => $products,
+            'user' => $users,
         ];
         return
         response()->json($data, 200);
     }
 
     // get all category list
-    public function categoryList(){
-        $category =  Category::get();
+    public function categoryList()
+    {
+        $category = Category::get();
 
         return response()->json($category, 200);
     }
 
     // create category
-public function categoryCreate (Request $request){
-    $data = [
-        'name' => $request->name,
-        'created_at' => Carbon::now(),
-        'updated_at' =>Carbon::now()
-    ];
-    $response =  Category::create($data);
-    return response()->json($response, 200);
-}
-     // category update
-     public function categoryUpdate(Request $request ){
+    public function categoryCreate(Request $request)
+    {
+        $data = [
+            'name' => $request->name,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ];
+        $response = Category::create($data);
+        return response()->json($response, 200);
+    }
+    // category update
+    public function categoryUpdate(Request $request)
+    {
 
         $categoryId = $request->category_id;
 
-        $dbData = Category::where('id',$categoryId)->first();
+        $dbData = Category::where('id', $categoryId)->first();
 
-        if(isset($dbData)){
-             $data = $this->getCategoryData($request);
-             $response = Category::where('id',$categoryId)->update($data);
-             return response()->json( ['status' => true ,'message'=> 'category update success' ,'category' => $response ], 200);
+        if (isset($dbData)) {
+            $data = $this->getCategoryData($request);
+            Category::where('id', $categoryId)->update($data);
+
+            $response = Category::where('id', $categoryId)->first();
+            return response()->json(['status' => true, 'message' => 'category update success', 'category' => $response], 200);
         }
-        return response()->json(['status' => false ,'message'=> 'no category avaliable' ,'category' => $response ], 500);
+        return response()->json(['status' => false, 'message' => 'no category avaliable'], 500);
     }
 
     // get category data
-    private function getCategoryData($request){
+    private function getCategoryData($request)
+    {
         return [
-            'name'=> $request->category_name,
-            'category_id' => $request->category_id,
+            'name' => $request->category_name,
             'created_at' => Carbon::now(),
-            'updated_at' =>Carbon::now(),
         ];
     }
 
 }
-
